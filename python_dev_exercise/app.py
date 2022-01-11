8da295439274480eb3896fe732f8d774c2071785
C.P. Santos <cpsanto@yahoo.com>
2022-01-11 09:51:43 -0700

    got rid of sandbox templates, made cp_requirements.pdf, requirements.txt and zip version of project to email, tested functionality. Everything seems to be working

--- python_dev_exercise/app.py
+++ python_dev_exercise/app.py
@@ -15,17 +15,25 @@ def home():
 def search():
 
     if request.method == 'POST':
+        #takes name field from form and assigns to query_name
         query_name = request.form['name']
+        #lowers query_name string to capture all matches whether or not string is capitalized
         query_name = query_name.lower()
+        #reads in patient information dataframe
         df = pd.read_csv('data/patient_tb.csv')
+        #lowers PatientFirstName column to match lower string of query name
         df['PatientFirstName']= df['PatientFirstName'].str.lower()
+        #filters dataframe for only matches of query_name and 'PatientFirstName' column
         result_df = df[df['PatientFirstName']== query_name]
+        #removes duplicates rows if matched on subset columns
         result_df2 = result_df.drop_duplicates(subset=['PatientFirstName', 'PatientLastName', 'TestName', 'MostRecentTestDate'])
-
-        if not result_df2.empty:
-            return render_template("search.html", tables = [result_df2.to_html(classes='table-striped table-dark')])
-        else: 
+        #returns rendered html with sliced dataframe and dropped duplicates
+        #if nothing is entered, entire dataframe is returned to see names to search on
+        if query_name == '':
             return render_template("search.html", tables = [df.to_html(classes='table-striped table-dark')])
+        #returns rendered html with sliced dataframe and dropped duplicates
+        else:
+            return render_template("search.html", tables = [result_df2.to_html(classes='table-striped table-dark')])
     
     
     
--- python_dev_exercise/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-# df = pd.read_csv('../data/patient_tb.csv')
-df = pd.read_csv('data/patient_tb.csv')
-first_name = df['PatientFirstName']
-print(first_name)
